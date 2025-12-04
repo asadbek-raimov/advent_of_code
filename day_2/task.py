17301aa1a03b4f084038a7335ee84b0a0f583298
Asadbek Raimov <asadbek.r@payme.uz>
2025-12-04 17:13:06 +0500

    day2: part 2 completed

--- day_2/task.py
+++ day_2/task.py
@@ -17,9 +17,34 @@ class Solution:
                     if len(curr) % 2 == 0:
                         if curr[:int(len(curr)/2)] == curr[int(len(curr)/2):]:
                             invalid_ids_sum += int(curr)
+
+    def find_invalid_ids_2(self):
+        invalid_ids_sum = 0
+        with open("./day_2/input.txt", "r") as file:
+            ranges = file.readline().split(",")
+            for elem in ranges:
+                start = int(elem.split("-")[0])
+                end = int(elem.split("-")[1])
+                
+                curr = str(end)
+                progress = str()
+                for i in range(len(curr) // 2):
+                    progress += curr[i]
+                    if curr.replace(progress, str()) == str():
+                        invalid_ids_sum += int(curr)
+                        break
+                
+                for i in range(end - start):
+                    curr = str(start + i)
+                    progress = str()
+                    for i in range(len(curr) // 2):
+                        progress += curr[i]
+                        if curr.replace(progress, str()) == str():
+                            invalid_ids_sum += int(curr)
+                            break
             
         print(invalid_ids_sum)
             
         
         
-Solution().find_invalid_ids()
+Solution().find_invalid_ids_2()
